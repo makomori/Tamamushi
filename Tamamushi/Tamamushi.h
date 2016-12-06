a2c63774ffeb4a6d1676ad1dcc6f09cde27633d3
makomori <makomori26@gmail.com>
2016-12-06 16:25:35 +0900

    create project

---
+++ Tamamushi/Tamamushi.h
@@ -0,0 +1,19 @@
+//
+//  Tamamushi.h
+//  Tamamushi
+//
+//  Created by 母利 睦人 on 2016/12/06.
+//  Copyright © 2016年 Femact Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Tamamushi.
+FOUNDATION_EXPORT double TamamushiVersionNumber;
+
+//! Project version string for Tamamushi.
+FOUNDATION_EXPORT const unsigned char TamamushiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Tamamushi/PublicHeader.h>
+
+
