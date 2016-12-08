e1dbe718bbbcbc56c9fe0102ee5a5d7e3ce179b8
makomori <makomori26@gmail.com>
2016-12-08 17:14:54 +0900

    add horizontal gradient nav bar method

--- Tamamushi/TMGradientNavigationBar.swift
+++ Tamamushi/TMGradientNavigationBar.swift
@@ -8,42 +8,45 @@
 
 import UIKit
 
-
 public enum Direction {
     case vertical
     case horizontal
-
-    func startPoint() -> CGPoint {
-        switch self {
-        case .vertical: return CGPoint(x:0, y:0)
-        case .horizontal: return CGPoint(x:0, y:0)
-        }
-    }
-
-    func endPoint() -> CGPoint {
-        switch self {
-        case .vertical: return CGPoint(x:0, y:0)
-        case .horizontal: return CGPoint(x:0, y:0)
-        }
-    }
 }
 
 public class TMGradientNavigationBar: NSObject {
-    var colors:[TMColor] = []
+    private var gradientColors:[TMColor] = []
 
     public func setBarGradientColor(direction: Direction, typeName: String) {
+        loadColors()
+        let matchedGradientColors = gradientColors.filter { $0.name == typeName }
+        if matchedGradientColors.count > 0 {
+            let gradientColor = matchedGradientColors[0]
+            if let startColor = gradientColor.startColor, let endColor = gradientColor.endColor {
+                setBacrGradientColor(direction: direction, startColor: startColor, endColor: endColor)
+            } else {
+                print("colors are not specified in JSON file")
+            }
+        } else {
+            print("specified type name doesn't match any of the gradient colors")
+        }
     }
 
     public func setBacrGradientColor(direction: Direction, startColor: UIColor, endColor: UIColor) {
-        UINavigationBar().setBackgroundImage(generateGradientImage(startColor: startColor, endColor: endColor), for: .default)
+        let image = generateGradientImage(direction: direction, startColor: startColor, endColor: endColor)
+        setImageToNavigationBar(image: image)
     }
 
-    func generateGradientImage(startColor: UIColor, endColor: UIColor) -> UIImage {
+    public func generateGradientImage(direction: Direction, startColor: UIColor, endColor: UIColor) -> UIImage {
         let gradientLayer = CAGradientLayer()
-        let width = UIScreen.main.bounds.size.width
-        let navBarFrame = CGRect(x: 0, y: 0, width: width, height: 64)
+        let sizeLength = UIScreen.main.bounds.size.height * 2
+        let navBarFrame = CGRect(x: 0, y: 0, width: sizeLength, height: 64)
         gradientLayer.frame = navBarFrame
-        gradientLayer.colors = [startColor, endColor]
+        gradientLayer.colors = [startColor.cgColor, endColor.cgColor]
+
+        if direction == .horizontal {
+            gradientLayer.startPoint = CGPoint(x: 0, y: 0.5)
+            gradientLayer.endPoint = CGPoint(x: 0.3, y: 0.5)
+        }
 
         UIGraphicsBeginImageContext(gradientLayer.frame.size)
         gradientLayer.render(in: UIGraphicsGetCurrentContext()!)
@@ -52,23 +55,30 @@ public class TMGradientNavigationBar: NSObject {
         return outputImage!
     }
 
-    func loadColors() {
-        if let path = Bundle.main.path(forResource: "gradients", ofType: "json") {
+    private func setImageToNavigationBar(image: UIImage) {
+        UINavigationBar.appearance().isOpaque = false
+        UINavigationBar.appearance().setBackgroundImage(image, for: .default)
+    }
+
+    private func loadColors() {
+        if let path = Bundle.init(for: TMGradientNavigationBar.self).path(forResource: "gradients", ofType: "json") {
             do {
                 let data = try NSData(contentsOfFile: path, options: NSData.ReadingOptions.mappedIfSafe)
-//                let json = JSON(data: data as Data)
-//                json.array?.forEach({ (eachJson) in
-//                    if let gradientColor = Mapper<GradientColor>().map(JSON: eachJson.dictionaryObject!) {
-//                        gradientColors.append(gradientColor)
-//                    }
-//                })
-//                collectionView?.reloadData()
-                if let parsedData = try? JSONSerialization.jsonObject(with: data as Data) {
-//                    print(parsedData["name"])
+                if let parsedData = try? JSONSerialization.jsonObject(with: data as Data) as! [[String: Any]] {
+                    parsedData.forEach({ (eachData) in
+                        let name = eachData["name"] as! String
+                        var colors = eachData["colors"] as! [String]
+                        let startColor = UIColor.hexStr(colors[0] as NSString, alpha: 1.0)
+                        let endColor = UIColor.hexStr(colors[1] as NSString, alpha: 1.0)
+                        let gradientColor = TMColor(startColor: startColor, endColor: endColor, name: name)
+                        gradientColors.append(gradientColor)
+                    })
                 }
             } catch {
                 print("error occured")
             }
+        } else {
+            print("no json file found")
         }
     }
 }
